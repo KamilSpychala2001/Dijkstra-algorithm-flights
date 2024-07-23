8e17176d5067a6a87cf3bb412cf689e3757bb9e5
Kamil <kamilspychala2001@gmail.com>
2024-07-23 23:24:12 +0200

    Add flight data preprocessing function

--- dijkstra.py
+++ dijkstra.py
@@ -6,8 +6,15 @@ pd.set_option('max_colwidth', 150)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 1000)
 
-
 # flights = pd.read_csv('./flights.csv')
 # flights = pd.read_csv('./flights_2.csv')
 # flights = pd.read_csv('flights_3.csv')
 flights = pd.read_csv('flights_4.csv')
+
+
+def preprocess_flights(flights):
+    flights['Departure datetime'] = pd.to_datetime(flights['Departure date'] + ' ' + flights['Departure time'])
+    flights['Arrival datetime'] = pd.to_datetime(flights['Arrival date'] + ' ' + flights['Arrival time'])
+
+    flights = flights.drop(['Departure date', 'Departure time', 'Arrival date', 'Arrival time'], axis=1)
+    return flights
