7ccb577f983538d5f3cb3112158676043e4216c3
Kamil <kamilspychala2001@gmail.com>
2024-07-23 23:23:28 +0200

    Load flight data from CSV file

--- dijkstra.py
+++ dijkstra.py
@@ -5,3 +5,9 @@ pd.set_option('display.max_columns', 1000)
 pd.set_option('max_colwidth', 150)
 pd.set_option('display.max_rows', None)
 pd.set_option('display.width', 1000)
+
+
+# flights = pd.read_csv('./flights.csv')
+# flights = pd.read_csv('./flights_2.csv')
+# flights = pd.read_csv('flights_3.csv')
+flights = pd.read_csv('flights_4.csv')
