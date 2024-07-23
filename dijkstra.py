5f938c054d1886721c7bb6bfc61402f6985b5290
Kamil <kamilspychala2001@gmail.com>
2024-07-23 23:22:54 +0200

    Initialize project and configure pandas display options

---
+++ dijkstra.py
@@ -0,0 +1,7 @@
+from datetime import datetime
+import pandas as pd
+
+pd.set_option('display.max_columns', 1000)
+pd.set_option('max_colwidth', 150)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', 1000)
